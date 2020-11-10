5969d7132188dc2bae0d38e106b0e0b67aa8e51a
wangdongmei <wdm_wangdongmeizju@zju.edu.cn>
2020-11-10 17:22:19 +0800

    up

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd /wangdongmei/nlp/text_summarization/BertSum/src
+python train.py -mode train -encoder classifier -dropout 0.1 -bert_data_path ../bert_data/cnndm -model_path ../models/bert_classifier -lr 2e-3 -visible_gpus 0  -gpu_ranks 0 -world_size 1 -report_every 50 -save_checkpoint_steps 1000 -batch_size 3000 -decay_method noam -train_steps 50000 -accum_count 2 -log_file ../logs/bert_classifier -use_interval true -warmup_steps 10000
+
