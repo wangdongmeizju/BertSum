d59736fd93ae3345fb15fe92b80d8ac6e9e6bc13
wangdongmei <wdm_wangdongmeizju@zju.edu.cn>
2020-11-24 17:33:17 +0800

    up

--- src/test.py
+++ src/test.py
@@ -2,6 +2,7 @@ import os
 import torch
 from others.logging import logger, init_logger
 if "__name__"  == "__main__":
+    init_logger("../logs/cnndm.log")
     print(torch.cuda.is_available())
     print(torch.cuda.device_count())
     os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3,4,5,6,7"
