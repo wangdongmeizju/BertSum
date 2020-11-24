ffd992033dcff6ab8bb030b5f6b4952c6b5c8d14
wangdongmei <wdm_wangdongmeizju@zju.edu.cn>
2020-11-24 17:12:18 +0800

    up

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd /wangdongmei/nlp/text_summarization/BertSum/src
 #single
-python train.py -mode train -encoder classifier -dropout 0.1 -bert_data_path ../bert_data/cnndm -model_path ../models/bert_classifier -lr 2e-3 -visible_gpus 0  -gpu_ranks 0 -world_size 1 -report_every 50 -save_checkpoint_steps 1000 -batch_size 3000 -decay_method noam -train_steps 50000 -accum_count 2 -log_file ../logs/bert_classifier -use_interval true -warmup_steps 10000
+#python train.py -mode train -encoder classifier -dropout 0.1 -bert_data_path ../bert_data/cnndm -model_path ../models/bert_classifier -lr 2e-3 -visible_gpus 0  -gpu_ranks 0 -world_size 1 -report_every 50 -save_checkpoint_steps 1000 -batch_size 3000 -decay_method noam -train_steps 50000 -accum_count 2 -log_file ../logs/bert_classifier -use_interval true -warmup_steps 10000
 #一台机器8个gpu卡
 #python train.py -mode train -encoder classifier -dropout 0.1 -bert_data_path ../bert_data/cnndm -model_path ../models/bert_classifier -lr 2e-3 -visible_gpus 0,1,2,3,4,5,6,7  -gpu_ranks 0,1,2,3,4,5,6,7 -world_size 8 -report_every 50 -save_checkpoint_steps 1000 -batch_size 3000 -decay_method noam -train_steps 50000 -accum_count 2 -log_file ../logs/bert_classifier -use_interval true -warmup_steps 10000
 #三台机器24个gpu卡
@@ -12,3 +12,4 @@ python train.py -mode train -encoder classifier -dropout 0.1 -bert_data_path ../
 
 #查看cuda信息
 nvidia-smi
+python test.py
--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-
+import os
 import torch
 if "__name__"  == "__main__":
+    print(torch.cuda.is_available())
+    print(torch.cuda.device_count())
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3,4,5,6,7"
     print(torch.cuda.is_available())
     print(torch.cuda.device_count())
