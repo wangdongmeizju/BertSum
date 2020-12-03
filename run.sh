7d0d1c092679917c873a630dd33e398b110a9f5b
wangdongmei <wdm_wangdongmeizju@zju.edu.cn>
2020-12-03 10:50:46 +0800

    up

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ cd /wangdongmei/nlp/text_summarization/BertSum/src
 #python preprocess.py -mode format_to_lines -raw_path ../merged_stories_tokenized -save_path ../json_data/cnndm -map_path ../urls -lower
 
 # greedy算法 每一句话先找出和摘要最相关的句子,然后用这个句子+第二个句子 与摘要比 找到第二个句子 以此类推找三个句子 combination 是直接组合找到最优,分数是精度+召回
-python preprocess.py -mode format_to_bert -raw_path ../json_data -save_path ../bert_data_bak -oracle_mode combination -n_cpus 4 -log_file ../logs/preprocess.log
+#python preprocess.py -mode format_to_bert -raw_path ../json_data -save_path ../bert_data_bak -oracle_mode combination -n_cpus 4 -log_file ../logs/preprocess.log
 
 #备注: self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True) 这个用的是bert-base-uncased do_lower_case 每篇文章最多100句,每句最多200个单词 长度截止为510
 # 数据生成 src 对应的token ;label每句话对应的标签 ;segs:每个token对应的句子id 奇数0 偶数为1;clss:CLS句子标志对应的位置;src_txt:原始文件 [句1,句2];tgt_txt:摘要句子
@@ -22,7 +22,7 @@ python preprocess.py -mode format_to_bert -raw_path ../json_data -save_path ../b
 #python train.py -mode validate -bert_data_path ../bert_data/cnndm -model_path ../models/bert_classifier  -visible_gpus 0  -gpu_ranks 0 -batch_size 30000  -log_file ../logs/bert_classifier_validate  -result_path ../results/cnndm -test_all -block_trigram true
 
 #预测
-python train.py -mode test -bert_data_path ../bert_data/cnndm -model_path ../models/bert_classifier  -visible_gpus 0  -gpu_ranks 0 -batch_size 30000  -log_file ../logs/bert_classifier_validate  -result_path ../test_results/cnndm -test_all -block_trigram true
+python train.py -mode test -test_from ../models/bert_classifier  -bert_data_path ../bert_data/cnndm -model_path ../models/bert_classifier  -visible_gpus 0  -gpu_ranks 0 -batch_size 30000  -log_file ../logs/bert_classifier_validate  -result_path ../test_results/cnndm -test_all -block_trigram true
 
 
 
