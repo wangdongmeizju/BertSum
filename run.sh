73a59f497aac793bf0390d99383d712e5c246529
wangdongmei <wdm_wangdongmeizju@zju.edu.cn>
2020-12-10 15:45:58 +0800

    up

--- run.sh
+++ run.sh
@@ -22,11 +22,11 @@ cd /wangdongmei/nlp/text_summarization/BertSum/src
 #python train.py -mode validate -bert_data_path ../bert_data/cnndm -model_path ../models/bert_classifier  -visible_gpus 0  -gpu_ranks 0 -batch_size 30000  -log_file ../logs/bert_classifier_validate  -result_path ../results/cnndm -test_all -block_trigram true
 
 #预测
-python train.py -mode test -test_from ../models/bert_classifier/model_step_49000.pt  -bert_data_path ../bert_data/cnndm -model_path ../models/bert_classifier  -visible_gpus 0  -gpu_ranks 0 -batch_size 30000  -log_file ../logs/bert_classifier_test  -result_path ../test_results/cnndm -test_all -block_trigram true
+#python train.py -mode test -test_from ../models/bert_classifier/model_step_49000.pt  -bert_data_path ../bert_data/cnndm -model_path ../models/bert_classifier  -visible_gpus 0  -gpu_ranks 0 -batch_size 30000  -log_file ../logs/bert_classifier_test  -result_path ../test_results/cnndm -test_all -block_trigram true
 
 
 
 
 #一台机器8个gpu卡
-#python train.py -mode train -encoder classifier -dropout 0.1 -bert_data_path ../bert_data/cnndm -model_path ../models/bert_classifier -lr 2e-3 -visible_gpus 0,1,2,3,4,5,6,7  -gpu_ranks 0,1,2,3,4,5,6,7 -world_size 8 -report_every 50 -save_checkpoint_steps 1000 -batch_size 3000 -decay_method noam -train_steps 50000 -accum_count 2 -log_file ../logs/bert_classifier -use_interval true -warmup_steps 10000
+python train.py -mode train -encoder classifier -dropout 0.1 -bert_data_path ../bert_data/cnndm -model_path ../models/bert_classifier -lr 2e-3 -visible_gpus 0,1,2,3,4,5,6,7  -gpu_ranks 0,1,2,3,4,5,6,7 -world_size 8 -report_every 50 -save_checkpoint_steps 1000 -batch_size 3000 -decay_method noam -train_steps 50000 -accum_count 2 -log_file ../logs/bert_classifier -use_interval true -warmup_steps 10000
 
